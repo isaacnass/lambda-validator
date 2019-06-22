8d94aab476e717592784e2dc6db253f3e192a76e
Isaac Nassimi <isaac@isaacnassimi.com>
2019-06-21 20:48:04 -0700

    Initial Commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+import { ValidatorOptions, Validator } from 'class-validator'
+
+const validator = new Validator()
+
+export default async function ValidateBody<T extends InstanceType<any>> (targetType: T, bodyString?: string | null, opts?: ValidatorOptions) {
+	const body = JSON.parse(bodyString || '')
+	const input = Object.assign(new targetType(), body)
+	const errors = await validator.validate(input, {
+		whitelist: true,
+	})
+	if (errors) {
+		return Promise.reject(errors)
+	}
+
+	return input
+}
